599ff6ba5cd96a06de88beab4f80576e16dc8fa6
buitrihieu98 <51129013+buitrihieu98@users.noreply.github.com>
2020-05-18 16:24:19 +0700

    +add pathdetail+ add subscription

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import Download from "./src/Components/Download/Download";
 import Search from "./src/Components/Search/Search";
 import Browse from "./src/Components/Browse/Browse";
 import CourseDetail from "./src/Components/Details/CourseDetail";
+import PathDetail from "./src/Components/Details/PathDetail";
+import Subscription from "./src/Components/Subscription/Subscription";
 
 
 export default function App() {
@@ -20,6 +22,11 @@ export default function App() {
   const author=[{id:1, username:'Hai Pham', avatar:''}]
   const course=
     {id:1, title: 'React Native', author: author , level:'Intermediate', releasedDate: 'July 2019', duration: '50 hours', rating : 4, ratingNumber: 406,}
+  const coursesList=[
+    {id:1, title: 'React Native', author: 'Hai Pham' , level:'Advance', releasedDate: 'July 2019', duration: '50 hours', rating : 4, ratingNumber: 406,},
+    {id:2, title: 'Java', author: 'Hai Pham' , level:'Beginner', releasedDate: 'July 2019', duration: '50 hours', rating : 5, ratingNumber: 709,},
+    {id:3, title: 'Game Development', author: 'ABC' , level:'Beginner', releasedDate: 'Sept 2019', duration: '50 hours', rating : 3, ratingNumber: 1307,}]
+  const path= {id:1, title: 'React Native', coursesNumber:12, coursesList :coursesList, progress:0}
 
   return (
       //<LoginComponent></LoginComponent>
@@ -34,6 +41,8 @@ export default function App() {
       //<Download></Download>
       //<Search></Search>
       //<Browse></Browse>
-      <CourseDetail item={course}></CourseDetail>
+      //<CourseDetail item={course}></CourseDetail>
+      //<PathDetail item={path}></PathDetail>
+      <Subscription></Subscription>
   );
 }
